7500722bbb00a89d7491b7b1bea1fd9a9062e09d
Irfan Aldiansyah <irfanaldi49@gmail.com>
2024-05-06 14:50:15 +0700

    Portofolio test

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import streamlit as st
 from PIL import Image
-
+#test
 
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
